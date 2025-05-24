6f82456777f463bab6e241580875e5830f8d07b3
Mxnalisa <amomalungs@gmail.com>
2025-05-24 13:44:32 +0200

    Updated displayTemperature function

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
-}
+  let dateElement = document.querySelector("#current-date");
+  let conditionElement = document.querySelector("#current-condition");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#current-wind-speed");
+  let now = new Date();
+  let formattedDate = now.toLocaleDateString(undefined, {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+  });
 
+  cityElement.innerHTML = response.data.city;
+  dateElement.innerHTML = formattedDate;
+  conditionElement.innerHTML = `, ${response.data.condition.description}`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = `${temperature}°`;
+}
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
 }
+let apiKey = "b2a5adcct04b33178913oc335f405433";
 
 function formatDate(date) {
   let minutes = date.getMinutes();

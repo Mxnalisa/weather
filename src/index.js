a23b653ce9b798c2b4dc7da2186266c8f23d0e26
Mxnalisa <amomalungs@gmail.com>
2025-05-24 15:28:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 }
 
 function search(event) {
